c64e42a8403132b3c8ad64fd51657c692ce20472
Juan <juandiego.velez@utp.edu.co>
2024-11-13 19:12:31 -0500

    Initial commit, crear configuración de tailwind y nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { colors, nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -5,14 +6,51 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "main-blue": "var(--main-blue)",
+        "main-blue-80": "var(--main-blue-80)",
+        "main-light-blue": "var(--main-light-blue)",
+        "main-dark-blue": "var(--main-dark-blue)",
+        "main-white": "var(--main-white)",
+        "main-gray": "var(--main-gray)",
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "var(--main-light-blue)",
+              foreground: colors.black,
+            },
+            secondary: {
+              DEFAULT: "var(--main-dark-blue)",
+              foreground: colors.black,
+            },
+          },
+        },
+        dark: {
+          colors: {
+            primary: {
+              DEFAULT: "var(--main-blue)",
+              foreground: colors.white,
+            },
+            secondary: {
+              DEFAULT: "#var(--main-white)",
+              foreground: colors.white,
+            },
+          },
+        },
+      },
+    }),
+  ],
 } satisfies Config;
